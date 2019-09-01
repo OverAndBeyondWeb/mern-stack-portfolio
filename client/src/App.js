376ae75ede460a749a2ff2a3f89ce6bafd40a474
Juan Fox <foxj0875@gmail.com>
2019-09-01 19:55:39 -0400

    Home component is displaying

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import graphic from './images/rebuild_graphic.jpg';
 import './App.scss';
+
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      
-      <h1>My Portfolio</h1>
+    <div className="App"> 
+      <Header />
+      <Home />
     </div>
   );
 }
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Home.scss';
+
+class Home extends Component {
+
+
+  render() { 
+    return (
+      <div>I'm home</div>
+    );
+  }
+}
+
+export default Home;
