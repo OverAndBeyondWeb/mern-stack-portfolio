3a1e3b0d154babac3873e5ea6fcabf8f8a4bf57c
Juan Fox <foxj0875@gmail.com>
2019-09-01 23:31:29 -0400

    Quote component is displaying

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,6 +3,7 @@ import './Home.scss';
 
 import LandingPage from '../LandingPage/LandingPage';
 import Skills from '../Skills/Skills';
+import Quote from '../Quote/Quote';
 
 class Home extends Component {
 
@@ -12,6 +13,7 @@ class Home extends Component {
       <div className="Home">
         <LandingPage />
         <Skills />
+        <Quote />
       </div>
       
     );
---
+++ client/src/components/Quote/Quote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Quote.scss';
+
+export default () => (
+  <div class="quote-container">
+    <div class="quote-two">
+      <p>"Relax knowing that your web development needs are the highest priority"</p>
+    </div>
+    <div class="divider-1"></div>
+    <div class="divider-2"></div>
+  </div>
+);
+
