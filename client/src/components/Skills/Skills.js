c489710c7f5fa603c729d5209be89c7652f15e22
Juan Fox <foxj0875@gmail.com>
2019-09-01 21:46:05 -0400

    Skills container is coded

---
+++ client/src/components/Skills/Skills.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Skills.scss';
+
+class Skills extends Component {
+  
+  state = {
+    skills: []
+  }
+
+  render() {
+    return (
+      <div class="skills">
+        
+      </div>
+    );
+  }
+}
