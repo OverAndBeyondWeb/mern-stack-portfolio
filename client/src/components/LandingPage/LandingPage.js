bec2fce6e6db2e53370f70894d2e4aaf5d271d22
Juan Fox <foxj0875@gmail.com>
2019-09-04 22:08:24 -0400

    new thumbnail images are being tracked now

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -6,7 +6,6 @@ import Header from '../Header/Header';
 export default () => (
   <div class="landing-page">
     <Header />
-    <img src="bgPhoto" alt=""/>
     <div class="page-container wrap">      
       <h1>full stack web developer</h1>      
       <p><em>Do you have a need for a devloper who just gets it?</em></p>       
