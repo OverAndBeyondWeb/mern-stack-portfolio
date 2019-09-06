a44fa999101cc2ba8e0114f1286e36c72d0e722b
Juan Fox <foxj0875@gmail.com>
2019-09-06 15:25:19 -0400

    React Router is added to the project and correctly displaying the Home component from the App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.scss';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App"> 
-      <Home />
-    </div>
+    <Router>
+      <div className="App"> 
+        <Route path="/" exact component={Home} />
+      </div>
+    </Router>
+    
   );
 }
 
